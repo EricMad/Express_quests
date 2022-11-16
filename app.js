0c20aff9ba375196488e0093beb1ff975b49e26b
EricMad <ermadou@gmail.com>
2022-11-16 16:18:48 +0100

    express 4bis done

--- app.js
+++ app.js
@@ -12,17 +12,20 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { validateMovie, validateUser } = require("./validators.js");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/users", movieHandlers.getUsers);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.get("/api/users/:id", movieHandlers.getUsersById);
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", movieHandlers.postUser);
 
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", movieHandlers.updateUser);
+app.get("/api/movies", validateMovie, movieHandlers.getMovies);
+app.get("/api/users", validateUser, movieHandlers.getUsers);
+app.get("/api/movies/:id", validateMovie, movieHandlers.getMovieById);
+app.get("/api/users/:id", validateUser, movieHandlers.getUsersById);
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, movieHandlers.postUser);
+
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, movieHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
